8b25ce6523670b78871c346e9da8d83156aa784e
samira1992 <samira_krdzic92@hotmail.com>
2023-07-01 17:12:25 +0200

    Adding empty state

--- src/components/Users/UsersList.js
+++ src/components/Users/UsersList.js
@@ -3,6 +3,20 @@ import Card from "../UI/Card";
 import classes from './UsersList.module.css'
 
 const UsersList = (props) => {
+
+  const { users } = props;
+
+  
+  if (users.length === 0) {
+    return (
+      <Card className={classes.users}>
+        <div className={classes.emptyListContainer}>
+          <p className={classes.emptyListText}>List is empty</p>
+        </div>
+      </Card>
+    );
+  }
+
   return (
     <Card className={classes.users}>
     <ul>
